054b905761ba40efa6837f65c99dcd2c668491ea
sastolarski <sastolarski@gmail.com>
2018-06-11 21:00:02 -0500

    spotify

--- liri.js
+++ liri.js
@@ -9,12 +9,19 @@ var request = require("request")
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
+var nodeArg = process.argv[2];
+
 console.log("hello")
 
-spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-    if (err) {
-      return console.log('Error occurred: ' + err);
-    }
-   
-  console.log(data); 
+
+spotify
+  .search({ 
+    type: 'track',
+    query: 'All the Small Things',
+    limit: 1
+ }).then(function(response) {
+    console.log(response.tracks.items);
+  })
+  .catch(function(err) {
+    console.log(err);
   });
