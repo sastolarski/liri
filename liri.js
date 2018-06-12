082e6f87b0ae80ac4273a59869807f25d4064f8b
sastolarski <sastolarski@gmail.com>
2018-06-12 12:14:31 -0500

    if statemnt for spotify and twitter

--- liri.js
+++ liri.js
@@ -14,34 +14,74 @@ var nodeArg = process.argv[2];
 console.log("-----------------------")
 
 
+// var songTitle = "Come, Become, To Be";
 
-spotify.search({ type: 'track', query: 'In Flames You Burn', limit: 3 }, function (error, data) {
-    if (!error) {
-        for (var i = 0; i < data.tracks.items.length; i++) {
-            var songData = data.tracks.items[i];
-            //artist
-            console.log("Artist: " + songData.artists[0].name);
-            //song name
-            console.log("Song: " + songData.name);
-            //spotify preview link
-            console.log("Preview URL: " + songData.preview_url);
-            //album name
-            console.log("Album: " + songData.album.name);
-            console.log("-----------------------");
+// spotify.search({ type: 'track', query: songTitle, limit: 1 }, function (error, data) {
+//     if (!error) {
+//         for (var i = 0; i < data.tracks.items.length; i++) {
+//             var songData = data.tracks.items[i];
+//             //artist
+//             console.log("Artist: " + songData.artists[0].name);
+//             //song name
+//             console.log("Song: " + songData.name);
+//             //spotify preview link
+//             console.log("Preview URL: " + songData.preview_url);
+//             //album name
+//             console.log("Album: " + songData.album.name);
+//             console.log("-----------------------");
 
+//         }
+//     } else {
+//         console.log('Error occurred.');
+//     }
+// });
+
+// var params = { screen_name: 'stevens91012116' };
+// client.get('statuses/user_timeline', params, function (error, tweets, response) {
+//     if (!error) {
+//         for (var i = 0; i < tweets.length; i++) {
+//             var date = tweets[i].created_at;
+//             console.log("Steven tweeted '" + tweets[i].text + "' On " + date.substring(0, 19));
+//             console.log("-----------------------")
+//         }
+//     } else {
+//         console.log('Error occurred.');
+//     }
+// });
+
+var command = process.argv[2];
+
+if (command === "spotify") {
+    var songTitle = "Come, Become, To Be";
+
+    spotify.search({ type: 'track', query: songTitle, limit: 1 }, function (error, data) {
+        if (!error) {
+            for (var i = 0; i < data.tracks.items.length; i++) {
+                var songData = data.tracks.items[i];
+                
+                console.log("Artist: " + songData.artists[0].name);   
+                console.log("Song: " + songData.name);          
+                console.log("Preview URL: " + songData.preview_url);            
+                console.log("Album: " + songData.album.name);
+                console.log("-----------------------");
+
+            }
+        } else {
+            console.log('Spotify Error');
         }
-    } else {
-        console.log('Error occurred.');
-    }
-});
+    })
 
-var params = { screen_name: 'stevens91012116' };
-client.get('statuses/user_timeline', params, function (error, tweets, response) {
-    if (!error) {
-        for (var i = 0; i < tweets.length; i++) {
-            var date = tweets[i].created_at;
-            console.log("Steven tweeted '" + tweets[i].text + "' On " + date.substring(0, 19));
-            console.log("-----------------------")
+} else if (command === "my-tweets") {
+    var params = { screen_name: 'stevens91012116' };
+    client.get('statuses/user_timeline', params, function (error, tweets, response) {
+        if (!error) {
+            for (var i = 0; i < tweets.length; i++) {
+                var date = tweets[i].created_at;
+                console.log("Steven tweeted '" + tweets[i].text + "' On " + date.substring(0, 19));
+                console.log("-----------------------")
+            }
+        } else {
+            console.log('Twitter Error');
         }
-    }
-});
+    })
+};
